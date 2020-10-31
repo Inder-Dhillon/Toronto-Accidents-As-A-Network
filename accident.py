a931e32c95a32e1c181c5dadba44e9ac2bbd461b
Inder Dhillon <inder_dhillon@outlook.com>
2020-10-31 02:58:40 -0400

    Adding accident dataclass

---
+++ accident.py
@@ -0,0 +1,11 @@
+from dataclasses import dataclass
+
+@dataclass
+class ACCIDENT:
+    """Class for an accident."""
+    acc_id: int
+    date: str
+    visibility: str
+    light: str
+    road_conditions: str
+    fatalities: int
